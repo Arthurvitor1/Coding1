02466893291cce56062f098a73374d2e4197719c
Arthurvitor1 <arthurvitor542@gmail.com>
2024-09-18 18:00:50 +0000

    criamos algoritimo

---
+++ aula.js
@@ -0,0 +1,24 @@
+// este código foi para treino de variáveis
+
+/*var rotulo = "natalya";
+rotulo = 12345;
+console.log(rotulo);
+*/
+/*const numero1 = 15;
+const numero2 = 20;
+const soma = numero1 + numero2;
+const subtracao = numero1 - numero2;
+const multiplicacao = numero1 * numero2;
+const divisao = numero1 / numero2
+console.log(`declare o resultado da soma:${soma}`);
+console.log(`declare o resultado da subsubtracao:${subtracao}`);
+console.log(`declare o resultado da multiplicacao: ${multiplicacao}`);
+console.log(`declare o resultado da divisao: ${divisao}`);
+*/
+
+const nota1 = 10;
+const nota2 = 7;
+const nota3 = 8;
+const media = (nota1+nota2+nota3)/3
+console.log(media)
+console.log('resultado da media é...${media}')
